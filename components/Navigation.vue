67757edbe3f9a3a3ed64ac9b786be825b8356f0f
HrishikeshKarale <Hrishikesh.karale@gmail.com>
2022-05-31 00:22:46 +0530

    Child routes added to existing navigaition

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,12 +1,56 @@
 <template>
-	<nav class="bg-white border-gray-200 px-8 sm:px-4 py-2.5 rounded dark:bg-gray-800">
-		<div class="container flex flex-wrap justify-between items-center mx-auto">
-			<a href="https://flowbite.com" class="flex items-center">
-			<img src="../assets/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo">
-			<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">UpStat</span>
-			</a>
+	<nav class="px-16 sm:px-4 py-4">
+		<div class="container flex flex-wrap justify-between items-center">
+			<div class="flex gap-16  items-end">
+				<a href="https://flowbite.com" class="flex items-center">
+					<img src="../assets/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo">
+					<span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Data Labs</span>
+				</a>
+				<div class="hidden justify-between items-center w-full md:flex md:w-auto" id="mobile-menu-4">
+					<ul class="flex flex-col gap-16 md:flex-row md:mt-0 md:text-sm md:font-medium">
+						<li
+							v-for="element in navigationLinks"
+							:key="element.component"
+							class="group"
+						>
+							<NuxtLink
+								class="flex flex-nowrap gap-2 items-center py-2 pr-4 pl-3 text-white dark:bg-blue-700 rounded md:bg-transparent md:p-0 dark:text-white"
+								aria-current="page"
+								:to="{name: element.component}"
+							>
+								{{ element.name }}
+								<font-awesome-icon 
+									v-if="element.child"
+									icon="fa-solid fa-chevron-down"
+									class="rotate-180 group-hover:rotate-0"
+								/>
+							</NuxtLink>
+							<div 
+								v-if="element.child"
+								class="hidden group-hover:block fixed bg-gray-200" 
+							>
+								<ul class="flex flex-col gap-4 px-4 py-3 md:mt-0 md:text-sm md:font-medium">
+									<li
+										v-for="el in element.child"
+										:key="el.component"
+									>
+										<NuxtLink
+											class="block py-2 pr-4 pl-3  text-gray-900 dark:bg-blue-700 rounded-lg md:bg-transparent md:p-0"
+											aria-current="page"
+											:to="{name: element.component+'-'+el.component}"
+										>
+											{{ el.name }}
+										</NuxtLink>
+									</li>
+								</ul>
+
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
 			<div class="flex md:order-2">
-			<button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+			<button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Get started</button>
 				<button data-collapse-toggle="mobile-menu-4" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-4" aria-expanded="false">
 					<span class="sr-only">
 						Open main menu
@@ -16,23 +60,6 @@
 					</svg>
 					<svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
 				</button>
-			</div>
-			<div class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-4">
-				<ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-
-					<li
-						v-for="element in navigationLinks"
-						:key="element.id"
-					>
-						<NuxtLink
-							class="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-							aria-current="page"
-							:to="{name: element.component}"
-						>
-							{{ element.name }}
-						</NuxtLink>
-					</li>
-				</ul>
 			</div>
 		</div>
 	</nav>
@@ -47,17 +74,31 @@ export default Vue.extend({
 
   data() {
     const navigationLinks: navigationLinkType[] = [{
-        id: 1,
         name: 'Home',
         component: 'index'
       },
+			{
+        name: 'Services',
+        component: 'services',
+				child: [{
+						name: 'BIOSTASTICS',
+						component: 'bioStat',
+					},
+					{
+						name: 'DATA MANAGEMENT',
+						component: 'dataMgmt',
+					},
+					{
+						name: 'STASTICAL PROGRAMMING',
+						component: 'statProg',
+					}
+				]
+      },
       {
-        id: 2,
         name: 'About',
         component: 'about'
       },
       {
-        id: 3,
         name: 'Contact',
         component: 'contact'
       },
