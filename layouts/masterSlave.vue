a677b930411f8833a057428346cab1ae28947dc8
HrishikeshKarale <Hrishikesh.karale@gmail.com>
2022-05-31 00:22:54 +0530

    banner added

--- layouts/masterSlave.vue
+++ layouts/masterSlave.vue
@@ -1,9 +1,27 @@
 <template>
 	<div class="masterSlaveLayout flex flex-col gap-8">
 		<!-- master -->
-		<header>
-			<Navigation />
-		</header>
+		<div class="flex flex-col sticky top">
+			<!-- banner -->
+			<div class="flex flex-row-reverse gap-8 px-4 h-48px bg-orange-500  text-white">
+				<div class="flex flex-row flex-nowrap gap-2 items-center">
+					<font-awesome-icon icon="fa-brands fa-twitter" class="p-2" style="font-size: 24px;"/>
+
+					<font-awesome-icon icon="fa-brands fa-instagram"  class="p-2" style="font-size: 24px;"/>
+				</div>
+
+				<address class="flex items-center">
+					call now:
+					<a href="tel:585-520-7382" class="p-3 text-bold"> ( 585 ) 520-7382 </a>
+				</address>
+			</div>
+
+			<!-- navigation -->
+			<header class="bg-stone-800">
+				<Navigation />
+			</header>
+		</div>
+
 		<!-- slave -->
 		<main class="flex flex-col gap-4">
 			<Nuxt />
