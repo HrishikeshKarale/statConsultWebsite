11b6a6b1487ea008c9e54bb826b3d8122a0a45af
HrishikeshKarale <Hrishikesh.karale@gmail.com>
2022-05-31 13:37:24 +0530

    page title cleanup

--- pages/services/index.vue
+++ pages/services/index.vue
@@ -1,22 +1,31 @@
 <template>
-  <div>
-    I am services
-    <ul class="flex flex-col gap-16 md:flex-row md:mt-0 md:text-sm md:font-medium">
-      <li
-        v-for="element in child"
-        :key="element.component"
-      >
-        <NuxtLink
-          class="flex flex-nowrap gap-2 items-center py-2 pr-4 pl-3  dark:bg-blue-700 rounded md:bg-transparent md:p-0"
-          aria-current="page"
-          :to="{name: 'services-'+element.component}"
-        >
-          {{ element.name }}
-        </NuxtLink>
-      </li>
-    </ul>
+  <article>
+			<h1 class="flex flex-row flex-nowrap items-center gap-4 h-32 px-16 font-medium leading-tight text-5xl m-0 text-gray-800 bg-gradient-to-r from-orange-500 to-orange-200"><span class="font-bold">Services</span>We Offer</h1>
+			<p class="text-base font-light leading-relaxed mt-0 mb-4 px-16">
+				Visit us or simply send us an email anytime you want. If you have any questions, please feel free to contact us.
+			</p>
+      <nav>
+        <ul class="flex flex-col gap-8 md:flex-row md:mt-0 md:text-sm md:font-medium px-16">
+          <li
+            v-for="element in child"
+            :key="element.component"
+          >
+            <NuxtLink
+              class="flex flex-nowrap gap-2 items-center py-2 pr-4 pl-3  dark:bg-blue-700 rounded md:bg-transparent md:p-0"
+              aria-current="page"
+              :to="{name: 'services-'+element.component}"
+            >
+              {{ element.name }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </nav>
+
+			<section class="flex flex-row flex-nowrap  justify-start gap-16 px-16">
+			</section>
+
     <nuxt-child />
-  </div>  
+  </article>  
 </template>
 
 <script lang="ts">
